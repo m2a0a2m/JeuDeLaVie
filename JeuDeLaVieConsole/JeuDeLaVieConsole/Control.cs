00592b3e83fb391eee58690cc9fc74dd4b448353
m2a0a2m <matt30.ca5a13@gmail.com>
2026-05-08 16:55:04 +0200

    main program

--- JeuDeLaVieConsole/JeuDeLaVieConsole/Control.cs
+++ JeuDeLaVieConsole/JeuDeLaVieConsole/Control.cs
@@ -2,73 +2,85 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace JeuDeLaVieConsole
 {
     internal class Control
     {
+        
         public static int[,] CreerGrille()
         {
-            int[,] grille = new int [Console.WindowWidth, Console.WindowHeight];
-                for (int i = 0; i < Console.WindowWidth; i++)
+            int[,] grille = new int [Console.WindowHeight-1, Console.WindowWidth-1];
+                for (int i = 0; i < Console.WindowHeight - 1; i++)
                 {
-                    for (int j = 0; j < Console.WindowHeight; j++)
+                    for (int j = 0; j < Console.WindowWidth - 1; j++)
                     {
                         grille[i, j] = 0;
                     }
                 }
                 return grille;
         }
-        public static void AfficherMenu()
+        public static int AfficherMenu()
         {
             Console.WriteLine("=====MENU=====");
             Console.WriteLine("1. ship");
+            Console.WriteLine("2. tris");
 
-            string forme = Console.ReadLine();
+            int forme =Convert.ToInt32(Console.ReadLine());
             Console.Clear();
 
             return forme;
         }
-        public static int[,] FaireShip(string forme, int[,] grille)
+        public static int[,] FaireShip(int forme, int[,] grille)
         {
             switch(forme)
             {
-                case "1":
+                case 1:
                     grille[1,3] = 1;
                     grille[2,1] = 1;
                     grille[2,3] = 1;
                     grille[3,2] = 1;
                     grille[3,3] = 1;
                     break;
+                case 2:
+                    grille[1, 1] = 1;
+                    grille[2, 1] = 1;
+                    grille[2, 1] = 1;
+                    break;
+
             }
             return grille;        
         }
         public static void AfficherGrille(int[,] grille)
         {
-            for (int i = 0; i < Console.WindowWidth; i++)
+            
+            for (int i = 0; i < Console.WindowHeight-1; i++)
             {
-                for (int j = 0; j < Console.WindowHeight; j++)
+                for (int j = 0; j < Console.WindowWidth-1; j++)
                 {
                     if (grille[i, j] == 1)
                     {
-                        Console.Write("#");
+                        Console.Write("█");
                     }
                     else
                     {
                         Console.Write(" ");
                     }
+                    
                 }
                 Console.WriteLine("");
             }
+            
         }
             public static bool VerifierLongueur(int[,] grille, int j,int i)
             {
-                if (i < 1 || j<1 || i>Console.WindowHeight-1 || j>Console.WindowWidth-1)
+                if (i <1 || j<1 || i>Console.WindowHeight-1 || j>Console.WindowWidth-1)
                 {
-                    return true;
+                    return false;
                 }
-                return false;
+                return true;
             }
     }
 }
--- JeuDeLaVieConsole/JeuDeLaVieConsole/Program.cs
+++ JeuDeLaVieConsole/JeuDeLaVieConsole/Program.cs
@@ -1,13 +1,115 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
+
+using System.Threading;
+
 namespace JeuDeLaVieConsole
 {
-    internal class Program
+    public class Program
     {
-        
+        static void Main(string[] args)
+        {
+            
+            Console.CursorVisible = false;
+            int choixForme = Control.AfficherMenu();
+            int[,] grille = Control.CreerGrille();
+            grille = Control.FaireShip(choixForme, grille);
+            bool fin = false;
+            
+
+            while (!fin)
+            {
+                Thread.Sleep(1000);
+
+                Console.Clear();
+                
+
+                for (int i = 0; i < grille.GetLength(0); i++)
+                {
+                    
+                    for (int j = 0; j < grille.GetLength(1); j++)
+                    {
+
+                        if (Control.VerifierLongueur(grille, j, i))
+                        {
+                            int nbAdjacents = 0;
+                            
+                            
+                                if (grille[i - 1, j - 1] == 1)
+                                {
+                                    nbAdjacents++;
+                                }
+                                if (grille[i - 1, j] == 1)
+                                {
+                                    nbAdjacents++;
+                                }
+                                if (grille[i - 1, j + 1] == 1)
+                                {
+                                    nbAdjacents++;
+                                }
+                                if (grille[i, j - 1] == 1)
+                                {
+                                    nbAdjacents++;
+                                }
+                                if (grille[i, j + 1] == 1)
+                                {
+                                    nbAdjacents++;
+                                }
+                                if (grille[i + 1, j - 1] == 1)
+                                {
+                                    nbAdjacents++;
+                                }
+                                if (grille[i + 1, j] == 1)
+                                {
+                                    nbAdjacents++;
+                                }
+                                if (grille[i + 1, j + 1] == 1)
+                                {
+                                    nbAdjacents++;
+                                }
+
+                                if (nbAdjacents != 0)
+                                {
+                                   Thread.Sleep(1000);
+                                }
+                            
+
+                            if (j != -1 && i != -1)
+                            {
+                                if (nbAdjacents == 3)
+                                {
+                                    grille[i, j] = 1;
+                                }
+                                else if (nbAdjacents < 2 || nbAdjacents > 3)
+                                {
+                                    grille[i, j] = 0;
+                                }
+                                else if (nbAdjacents == 2)
+                                {
+                                    grille[i, j] = grille[i, j];
+                                }
+                            }
+                            
+
+                        }
+                        Console.Clear();
+                        Control.AfficherGrille(grille);
+                        
+
+
+                    }
+                    
+                }
+                
+                
+
+            }
+        }
     }
 }
+
